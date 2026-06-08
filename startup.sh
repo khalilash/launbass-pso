46db43154e0897c27f65d70bebc598110b447dab
nabilaakn <nabilarahadatul16@gmail.com>
2026-06-08 15:50:20 +0700

    Fix nginx laravel routing

--- startup.sh
+++ startup.sh
@@ -6,7 +6,30 @@ php artisan config:clear || true
 php artisan route:clear || true
 php artisan view:clear || true
 
-sed -i 's#root /home/site/wwwroot;#root /home/site/wwwroot/public;#g' /etc/nginx/sites-available/default
-sed -i 's#try_files \$uri \$uri/ =404;#try_files \$uri \$uri/ /index.php?\$args;#g' /etc/nginx/sites-available/default
+cat > /etc/nginx/sites-available/default <<'EOF'
+server {
+listen 8080;
+listen [::]:8080;
 
-service nginx reload
+```
+root /home/site/wwwroot/public;
+index index.php index.html index.htm;
+
+location / {
+    try_files $uri $uri/ /index.php?$query_string;
+}
+
+location ~* [^/]\.php(/|$) {
+    fastcgi_split_path_info ^(.+?\.[Pp][Hh][Pp])(|/.*)$;
+    fastcgi_pass 127.0.0.1:9000;
+    include fastcgi_params;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    fastcgi_param PATH_INFO $fastcgi_path_info;
+}
+```
+
+}
+EOF
+
+service nginx restart
+php-fpm
