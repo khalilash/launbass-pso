4b66509dca6677eb819d14abdc9e5b8bdddc8849
jajaskikiyaya <162094899+zsky-le@users.noreply.github.com>
2026-06-08 16:48:06 +0700

    Refactor Nginx configuration in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
-
 cd /home/site/wwwroot
-
 php artisan config:clear || true
 php artisan route:clear || true
 php artisan view:clear || true
-
+chmod -R 775 /home/site/wwwroot/storage
+chmod -R 775 /home/site/wwwroot/bootstrap/cache
 cat > /etc/nginx/sites-available/default <<'EOF'
 server {
-listen 8080;
-listen [::]:8080;
-
-```
-root /home/site/wwwroot/public;
-index index.php index.html index.htm;
-
-location / {
-    try_files $uri $uri/ /index.php?$query_string;
-}
-
-location ~* [^/]\.php(/|$) {
-    fastcgi_split_path_info ^(.+?\.[Pp][Hh][Pp])(|/.*)$;
-    fastcgi_pass 127.0.0.1:9000;
-    include fastcgi_params;
-    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    fastcgi_param PATH_INFO $fastcgi_path_info;
-}
-```
-
+    listen 8080;
+    listen [::]:8080;
+    root /home/site/wwwroot/public;
+    index index.php index.html index.htm;
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+    location ~* [^/]\.php(/|$) {
+        fastcgi_split_path_info ^(.+?\.[Pp][Hh][Pp])(|/.*)$;
+        fastcgi_pass 127.0.0.1:9000;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param PATH_INFO $fastcgi_path_info;
+    }
 }
 EOF
-
 service nginx restart
 php-fpm
